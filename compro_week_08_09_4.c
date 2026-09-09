31ad545992677ff940252b29f3a3bbe03f7b223f
s6912041610779-Lalida <s6912041610779@email.kmutnb.ac.th>
2026-09-09 18:03:19 +0000

    week_08_09

---
+++ compro_week_08_09_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num ;
+    int isPrime = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        isPrime = 0;
+    }else {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0){
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+if (isPrime) {
+    printf("%d is a prime number.\n", num);
+}else {
+    printf("%d is not a prime number.\n", num);
+}
